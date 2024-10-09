672642f70bb80f946fb6236825146a2601f21d4a
Danilo <damilhome@gmail.com>
2024-10-09 17:02:45 -0300

    tocando som ao clicar em cada tecla

---
+++ src/scripts/engine.js
@@ -0,0 +1,12 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+let audio = new Audio()
+
+const playTune = (key) => {
+    audio.src = `./src/tunes/${key}.wav`;
+    audio.play()
+};
+
+pianoKeys.forEach(key => {
+    key.addEventListener('click', () => playTune(key.dataset.key));
+})
